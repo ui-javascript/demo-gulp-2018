830b1235f1830fb41754deef729965a1bee8cadc
luo0412 <1095847440@qq.com>
2019-05-23 19:19:33 +0800

    small change

---
+++ _mvvm/js/react-antd.js
@@ -0,0 +1,14 @@
+// import {Button, Radio, Icon} from 'antd';
+
+ReactDOM.render(
+    <div>
+        <div>
+            <antd.Button type="primary">Primary</antd.Button>
+            <antd.Button>Default</antd.Button>
+            <antd.Button type="dashed">Dashed</antd.Button>
+            <antd.Button type="danger">Danger</antd.Button>
+        </div>
+
+    </div>,
+    document.getElementById('app2'),
+)
